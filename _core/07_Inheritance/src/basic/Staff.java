06842f3ccfa667c86a4b31894eaa6f541cd4ab2e
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-06-01 15:13:39 +0900

    Java 26
    learn Java

---
+++ _core/07_Inheritance/src/basic/Staff.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class Staff extends Person {
+	private String job;
+	
+	public static void main(String[] args) {
+		System.out.println("This is Staff Instance");
+	}
+	
+	public void printJob() {
+		System.out.println(super.getName() + " 직원의 업무는 " + job);
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+}
