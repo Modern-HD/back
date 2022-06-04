512a1db49cd5f986a3be74061edaf6ce96980d6c
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-06-05 00:27:18 +0900

    Java 32
    learn Java

---
+++ _core/10_Collection_Framework/src/etc/enums/Fares.java
@@ -0,0 +1,31 @@
+package etc.enums;
+
+public enum Fares {
+	BUS(100) {
+		int fare(int dist) {
+			return dist * BASE_FARE;
+		}
+	}, 
+	KTX(200) {
+		int fare(int dist) {
+			return dist * BASE_FARE;
+		}
+	}, 
+	AIR(300) {
+		int fare(int dist) {
+			return dist * BASE_FARE;
+		}
+	};
+	
+	protected final int BASE_FARE;
+	
+	private Fares(int baseFare) {
+		BASE_FARE = baseFare;
+	}
+	
+	public int getBaseFare() {
+		return BASE_FARE;
+	}
+	
+	abstract int fare(int dist);
+}
