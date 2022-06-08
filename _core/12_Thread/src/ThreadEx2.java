b2fd8226fea60cc0b2694528c504c4b7dc36aa86
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-06-09 00:08:56 +0900

    Java 37
    learn Java

---
+++ _core/12_Thread/src/ThreadEx2.java
@@ -0,0 +1,23 @@
+
+public class ThreadEx2 {
+	static long startTime = 0L;
+	
+	public static void main(String[] args) {
+		ThreaderA tha = new ThreaderA();
+		ThreaderB thb = new ThreaderB();
+		
+		tha.start();
+		thb.start();
+		startTime = System.currentTimeMillis();
+		try {
+			tha.join();
+			thb.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(System.currentTimeMillis() - ThreadEx2.startTime);
+		
+		// 싱크를 맞추려면 thread synchronized 이용
+	}
+	
+}
