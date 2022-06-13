63fce09de9512beb76a5dca64a45033bf962f37d
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-06-14 00:40:37 +0900

    Java 42
    learn Java

---
+++ _core/16_JDBC/src/product/DAO.java
@@ -0,0 +1,13 @@
+package product;
+
+import java.util.List;
+
+public interface DAO {
+
+	int insert(ProductVO pvo); // Data Access Object
+	List<ProductVO> selectList();
+	ProductVO selectOne(int pno);
+	int update(ProductVO pvo);
+	int delete(int pno);
+	
+}
---
+++ _core/16_JDBC/src/product/Service.java
@@ -0,0 +1,13 @@
+package product;
+
+import java.util.List;
+
+public interface Service {
+
+	int register(ProductVO pvo);
+	List<ProductVO> list();
+	ProductVO detail(int pno);
+	int edit(ProductVO pvo);
+	int remove(int pno);
+	
+}
