0b6da7447897cd641cb4577c359a866dae74932f
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-07-06 00:53:36 +0900

    JSP 15
    learn Jsp

---
+++ _jsp/04_final_jsp_crud/src/main/java/controller/CommentCtrl.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import domain.CommentVO;
+import service.CommentService;
+import service.CommentServiceImpl;
+
+@WebServlet("/com/*")
+public class CommentCtrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(CommentCtrl.class);
+	private RequestDispatcher rdp;
+	private CommentService csv;
+	private int isUp;
+	private String destPage;
+	
+	
+    public CommentCtrl() {
+    	csv = new CommentServiceImpl();
+    }
+
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		res.setCharacterEncoding("utf-8");
+		res.setContentType("text/html; charset=utf-8");
+		
+		String uri = req.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/")+1);
+		log.info(">>> path : " + path);
+		
+		switch (path) {
+		case "post":
+			break;
+		default:
+			break;
+		}
+		
+		rdp = req.getRequestDispatcher(destPage);
+		rdp.forward(req, res);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		service(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		service(request, response);
+	}
+
+}
