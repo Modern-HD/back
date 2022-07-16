2768c81b4d7c243d89a37778eb75534a9c84c62c
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-07-17 00:00:11 +0900

    JSP 26
    learn Jsp

---
+++ _jsp/06_final_jsp_fileUpload/src/main/java/repository/RepliesDAO.java
@@ -0,0 +1,14 @@
+package repository;
+
+import java.util.List;
+
+import domain.RepliesVO;
+
+public interface RepliesDAO {
+	public int insert(RepliesVO rvo);
+	public List<RepliesVO> selectList(int pno);
+	public RepliesVO selectOne(long rno);
+	public int update(RepliesVO rvo);
+	public int delete(long rno);
+	public int deleteAll(int pno);
+}
