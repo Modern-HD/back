f942e73128d8ee9ba105e3ff39d78773a7203f39
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-08-29 00:07:41 +0900

    Spring 40
    learn Spring

---
+++ _spring/50_paging/src/main/webapp/resources/js/product.detail.js
@@ -0,0 +1,9 @@
+if (document.getElementById('product_del_btn') != null) {
+    document.getElementById('product_del_btn').addEventListener('click', () => {
+        const del_form = document.getElementById('del_form');
+        const pno_val = document.getElementById('pno').innerText;
+        document.getElementById('input_pno').value = pno_val;
+        del_form.setAttribute("action", "/product/delete");
+        del_form.submit();
+    });
+}
