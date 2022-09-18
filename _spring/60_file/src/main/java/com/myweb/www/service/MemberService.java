10610f0ea422428e44b227f4a36d75955f73e85d
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2022-09-19 07:01:28 +0900

    Spring 59
    learn Spring

---
+++ _spring/60_file/src/main/java/com/myweb/www/service/MemberService.java
@@ -0,0 +1,18 @@
+package com.myweb.www.service;
+
+import java.util.List;
+
+import com.myweb.www.domain.FileVO;
+import com.myweb.www.domain.MemberVO;
+import com.myweb.www.domain.PagingVO;
+
+public interface MemberService {
+	int register(MemberVO mvo, FileVO fvo);
+	MemberVO login(MemberVO mvo);
+	List<MemberVO> getList(PagingVO pgvo);
+	MemberVO detail(String email);
+	int modify(MemberVO mvo);
+	int remove(String email);
+	int dupleChrck(String string);
+	int getTotalCount(PagingVO pgvo);
+}
